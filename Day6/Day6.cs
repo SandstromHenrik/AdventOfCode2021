493b248946745a5d734db86f4985e23b18ef99c3
Henrik Sandström <Henrik-Sandstrom@outlook.com>
2021-12-06 17:27:46 +0100

    Refactoring

--- Day6/Day6.cs
+++ Day6/Day6.cs
@@ -6,7 +6,9 @@ namespace Advent_of_Code_2021.Puzzles
 { 
     public class Day6
     {
-        private long[] DayCount = new long[10];
+        // Fish Count by day (pos 1-9)
+        // pos 0 does not represent a day and is used to keep track of newborns
+        private long[] FC = new long[10]; 
 
         public Day6()
         {
@@ -15,19 +17,17 @@ namespace Advent_of_Code_2021.Puzzles
                 .Split(',')
                 .ToList()
                 .ForEach(t => {
-                    DayCount[Int32.Parse(t)+1]++;
+                    FC[Int32.Parse(t)+1]++;
                 });
         }
 
-        public void Part1And2(int days, long ph = 0)
+        public long FishGrowth(int days)
         {
-            RangeAction(0, days, d => { 
-                RangeAction(0, DayCount.Length, t => ph = t < DayCount.Length-1 ? (DayCount[t] = DayCount[t+1]) : (DayCount[7] += DayCount[9] = DayCount[0]));
-            });
+            Enumerable.Range(0, days)
+                      .ToList()
+                      .ForEach(d => FC[7] = FC[8] + (FC[9] = FC.Take(9).Select((c, i) => FC[i] = FC[i+1]).ToArray()[0]));
 
-            Console.WriteLine($"How many lanternfish would there be after {days} days? - " + DayCount.Skip(1).Sum());
+            return FC.Skip(1).Sum();
         }
-
-        public void RangeAction(int s, int l, Action<int> a) => Enumerable.Range(s, l).ToList().ForEach(a);
     }
 }
