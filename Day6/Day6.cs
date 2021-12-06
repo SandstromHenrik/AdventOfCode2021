c164e28ea77a9a51847a7b43015e2ecf0aa9a697
Henrik Sandström <Henrik-Sandstrom@outlook.com>
2021-12-06 09:50:49 +0100

    Add day 6 solution

---
+++ Day6/Day6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2021.Puzzles
+{ 
+    public class Day6
+    {
+        private long[] TimerCount = new long[9];
+
+        public Day6()
+        {
+            File.ReadLines("Puzzles/Day6/input.txt")
+                .First()
+                .Split(',')
+                .ToList()
+                .ForEach(t => {
+                    TimerCount[Int32.Parse(t)]++;
+                });
+        }
+
+        public void Part1And2(int days)
+        {
+            for(int d = 0; d < days; d++)
+            {
+                long newCount = TimerCount[0];
+
+                for(int t = 1; t < TimerCount.Length; t++)
+                    TimerCount[t-1] = TimerCount[t];
+
+                TimerCount[6] += TimerCount[8] = newCount;
+            }
+
+            Console.WriteLine($"How many lanternfish would there be after {days} days? - " + TimerCount.Sum());
+        }
+
+
+    }
+}
